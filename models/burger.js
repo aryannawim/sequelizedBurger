7c4f0e978e0fb565c37c9ff757387541bb481014
aryannawim <aryannawim@gmail.com>
2017-02-22 17:41:41 -0600

    Add files via upload
    not complete...

---
+++ models/burger.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    {devoured: DataTypes.BOOLEAN, defaultValue: false}
+    
+  });
+  return Burger;
+};
